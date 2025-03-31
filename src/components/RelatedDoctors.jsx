7891910ff5d821e7e1abc59a61d5753bcb93ad80
Sappa Sujeeth <sappasujeeth123@gmail.com>
2025-03-31 21:33:49 +0530

    MERN Website

---
+++ src/components/RelatedDoctors.jsx
@@ -0,0 +1,18 @@
+import React, { useContext, useState } from 'react'
+import { AppContext } from '../context/AppContext'
+
+const RelatedDoctors = (speciality,docId) => {
+ 
+ const {doctors} =useContext(AppContext)
+ const [relDoc,setRealDoc] = useState([])  
+ 
+ return (
+    <div>
+    
+  
+
+    </div>
+  )
+}
+
+export default RelatedDoctors
