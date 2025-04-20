3f9d355b614ecd4a9510beb7464fd3d3609939fd
Sappa-Sujeeth <sappasujeeth123@gmail.com>
2025-04-20 12:29:17 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
      <div>
         <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
         <ul className='flex flex-col gap-2 text-gray-600'>
-            <li>+91-9519269289</li>
+            <li>+91-8978683569</li>
             <li>healthsure@gmail.com</li>
         </ul>
      </div>
