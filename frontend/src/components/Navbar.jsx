2f51398d73a67938bad6066413755a44c6a40851
Sappa-Sujeeth <sappasujeeth123@gmail.com>
2025-04-20 12:18:28 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
                     <li className='py-1'>CONTACT</li>
                     <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
                 </NavLink>
-                <a href='http://localhost:5174/doctor-list' className='border px-5 text-xs py-1.5 rounded-full'  >Admin Panel</a>
+                <a href='https://healthsure-admin.onrender.com' className='border px-5 text-xs py-1.5 rounded-full'  >Admin Panel</a>
 
             </ul>
             <div className='flex items-center gap-4'>
